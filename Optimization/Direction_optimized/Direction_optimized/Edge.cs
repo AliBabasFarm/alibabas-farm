f54f492e20278876e689cb2da139bc7818a1382c
Ali UÇAR <cengaliucar@gmail.com>
2017-10-22 23:58:58 +0300

    Kruskal Algorithm Test

---
+++ Optimization/Direction_optimized/Direction_optimized/Edge.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direction_optimized
+{
+    public class Edge
+    {
+        public string p1_name;
+        public string p2_name;
+        public double distance;
+        public Edge(string Name1, string Name2, double dist)
+        {
+            p1_name = Name1;
+            p2_name = Name2;
+            distance = dist;
+        }
+        public Edge()
+        { }
+    }
+}
+
--- Optimization/Direction_optimized/Direction_optimized/Form1.cs
+++ Optimization/Direction_optimized/Direction_optimized/Form1.cs
@@ -12,15 +12,87 @@ namespace Direction_optimized
 {
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+        List<Edge> map = new List<Edge>();
+        List<string> cities = new List<string>();
+        private void Form1_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Merhaba");
-            MessageBox.Show("Merhaba");
+            LoadSampleMap();
+            KruskalAlgoritm();
+            WritetoListbox();
+        }
+
+        private void WritetoListbox()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void KruskalAlgoritm()
+        {
+            Edge sample = new Edge();
+            List<Edge> Sortedmap = map.OrderBy(o =>o.distance).ToList();
+            List<Edge> SelectedEdge = new List<Edge>();
+            List<string> InsertedCity = new List<string>();
+            double distance = 0;
+
+            for(int i=0;i<Sortedmap.Count;i++)
+            {
+                if (InsertedCity.IndexOf(Sortedmap[i].p1_name) <= 0 || InsertedCity.IndexOf(Sortedmap[i].p2_name) <= 0)
+                {
+                    
+                    SelectedEdge.Add(Sortedmap[i]);
+                    if(InsertedCity.IndexOf(Sortedmap[i].p1_name) <= 0)
+                    InsertedCity.Add(Sortedmap[i].p1_name);
+                    if (InsertedCity.IndexOf(Sortedmap[i].p2_name) <= 0)
+                        InsertedCity.Add(Sortedmap[i].p2_name);
+                    distance += SelectedEdge[i].distance;
+
+                }
+                if (InsertedCity.Count == cities.Count)
+                    break;
+                
+            }
+
+
+
+
+
+            
+
+
+        }
+
+        private void LoadSampleMap()
+        {
+            cities.Add("A"); cities.Add("B"); cities.Add("C");
+            cities.Add("D"); cities.Add("E"); cities.Add("F");
+            cities.Add("G"); cities.Add("H"); 
+           
+
+            map.Add(new Edge("A", "B", 15));
+            map.Add(new Edge("B", "C", 10));
+            map.Add(new Edge("C", "H", 20));
+            map.Add(new Edge("F", "E",12));
+            map.Add(new Edge("A", "D", 20));
+            map.Add(new Edge("A", "F", 14));
+            map.Add(new Edge("F", "B", 8));
+            map.Add(new Edge("B", "E", 13));
+            map.Add(new Edge("E", "G", 14));
+            map.Add(new Edge("C", "G", 9));
+            map.Add(new Edge("G", "H", 15));
+            map.Add(new Edge("C", "D", 5));
+            map.Add(new Edge("E", "C", 16));
+            map.Add(new Edge("E", "H", 25));
+            map.Add(new Edge("A", "C", 18));
+
+
         }
     }
 }
